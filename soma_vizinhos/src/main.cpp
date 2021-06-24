041de185daa181fb1c5c88cf1a273ea5ac30b6b5
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-24 13:05:08 -0300

    Update main.cpp

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -8,9 +8,28 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
+int main(void)
 {
-    // TODO: Adicione seu código aqui. 
-
+    int m;
+    int n;
+    int sum;
+    while (cin >> std::ws >> m >> n) {
+        sum = m;
+        if(n>0){
+            n--;
+            for (; n > 0; n--) {
+                m++;
+                sum += m;
+            }
+        }
+        else if (n < 0) {
+            n++;
+            for (; n < 0; n++) {
+                m--;
+                sum += m;
+            }
+        }
+        cout << sum<<endl;
+    }
     return 0;
 }
