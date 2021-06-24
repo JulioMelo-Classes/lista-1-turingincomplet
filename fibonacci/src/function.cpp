c0658524ef8f7557de73491503b08acbb48f3a29
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-24 17:10:10 -0300

    fibonacci-C

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -2,8 +2,13 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+  std::vector<unsigned int> v;
+        int x = 1, y = 1, z = 0;
+        while( x < n) {
+            v.push_back(x);
+            z = x + y;
+            x = y;
+            y = z;
+    }
+    return v;
 }
