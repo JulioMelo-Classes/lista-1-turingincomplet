56e8964502468a2c7cfee7f9fe690f96b0ebe7c6
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-25 15:56:50 -0300

    retangulos1-C

--- autor.md
+++ autor.md
@@ -15,7 +15,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [x] Minmax
 - [x] Inverter
 - [ ] Filtragem
-- [ ] Ponto em Retângulo 1
+- [x] Ponto em Retângulo 1
 - [ ] Ponto em Retângulo 2
 
 
--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -4,10 +4,11 @@
  * Verifica se um ponto está dentro de um retângulo.
  */
 
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
+location_t pt_in_rect( const Ponto& IE, const Ponto& SD, const Ponto& P )
 {
-    // TODO: Coloque aqui seu código.
-
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+    if (IE.x > P.x || SD.x < P.x || IE.y > P.y || SD.y < P.y)
+        return OUTSIDE;
+    else if (IE.x < P.x && SD.x > P.x && IE.y < P.y && SD.y > P.y)
+        return INSIDE;
+    else return BORDER;
 }
