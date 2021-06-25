fc49935bb87afcb1027250e032801327c6174279
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-25 17:32:26 -0300

    entagulos2-C
    All done!

--- autor.md
+++ autor.md
@@ -16,7 +16,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [x] Inverter
 - [ ] Filtragem
 - [x] Ponto em Retângulo 1
-- [ ] Ponto em Retângulo 2
+- [x] Ponto em Retângulo 2
 
 
 --------
--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -21,7 +21,17 @@ using std::max;
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
+    Ponto r1, r2, p, ie, sd;
+    array<string, 4>sres = { "inside","border","outside","invalid" };
+    while (cin >> std::ws >> r1.x >> r1.y >> r2.x >> r2.y >> p.x >> p.y) {
+        if (r1.x == r2.x && r1.y == r2.y) {
+            cout << sres[3] << endl;
+            continue;
+        }
+        ie = { min(r1.x,r2.x),min(r1.y,r2.y) };
+        sd= { max(r1.x,r2.x),max(r1.y,r2.y) };
+        location_t r = pt_in_rect(ie, sd, p);
+        cout << sres[r] << endl;
+    }
     return 0;
 }
